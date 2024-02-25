b4be9f8fbbe78085cbc9c49c01aadb03fc053a58
NikhilIIITB <155606598+NikhilIIITB@users.noreply.github.com>
2024-02-25 14:39:20 +0530

    Update Calculator.java

--- scientific_calculator/src/main/java/org/spe/Calculator.java
+++ scientific_calculator/src/main/java/org/spe/Calculator.java
@@ -2,13 +2,33 @@ package org.spe;
 
 public class Calculator {
 
-    public int add (int num1 ,int num2)
-    {
-        return num1 + num2;
+
+//    public int add (int num1 ,int num2)
+//    {
+//        return num1 + num2;
+//    }
+//
+//    public int sub (int num1 ,int num2)
+//    {
+//        return num1 - num2;
+//    }
+
+    public double squareRoot(double num) {
+        if ( num<0) return -1;
+        return Math.sqrt(num);
+    }
+    public int factorial(int num) {
+        if ( num<0) return -1;
+        int ans =1;
+        for(int i=1;i<=num;i++) ans*=i;
+        return ans;
+    }
+    public double naturalLogarithm(double num) {
+        if ( num<0) return -1;
+        return Math.log(num);
     }
 
-    public int sub (int num1 ,int num2)
-    {
-        return num1 - num2;
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
     }
 }
