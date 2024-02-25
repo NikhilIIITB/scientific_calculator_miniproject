43cfa27becba6e364fd92129dd0d102ac178b57e
NikhilIIITB <155606598+NikhilIIITB@users.noreply.github.com>
2024-02-25 14:40:00 +0530

    Update calculatorTest.java

--- scientific_calculator/src/test/java/org/spe/calculatorTest.java
+++ scientific_calculator/src/test/java/org/spe/calculatorTest.java
@@ -6,15 +6,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class calculatorTest {
 
+    //    @Test
+//    void add() {
+//        Calculator cal = new Calculator();
+//        assertEquals(10,cal.add(7,3));
+//    }
+//
+//    @Test
+//    void sub() {
+//        Calculator cal = new Calculator();
+//        assertEquals(4,cal.sub(7,3));
+//    }
+    Calculator calculator=new Calculator();
     @Test
-    void add() {
-        Calculator cal = new Calculator();
-        assertEquals(10,cal.add(7,3));
+    void squareRoot() {
+
+        assertEquals(2.0, calculator.squareRoot(4)); // Test square root of 4
+        assertEquals(3.0, calculator.squareRoot(9)); // Test square root of 9
+        assertEquals(4.0, calculator.squareRoot(16)); // Test square root of 16
     }
 
     @Test
-    void sub() {
-        Calculator cal = new Calculator();
-        assertEquals(4,cal.sub(7,3));
+    void factorial() {
+        assertEquals(1,calculator.factorial(0));
+        assertEquals(1,calculator.factorial(1));
+        assertEquals(24,calculator.factorial(4));
+    }
+
+    @Test
+    void naturalLogarithm() {
+        assertEquals(0,calculator.naturalLogarithm(1));
+    }
+
+    @Test
+    void power() {
+        assertEquals(4,calculator.power(2,2));
+        assertEquals(27,calculator.power(3,3));
     }
 }
